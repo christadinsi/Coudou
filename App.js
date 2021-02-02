be49d66b445eef1ce3d5bc326a72cc7c30985e68
christ adinsi <christadindi@gmail.com>
2021-02-02 12:47:23 +0100

    message

--- App.js
+++ App.js
@@ -1,11 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View,TextInput} from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+       <TextInput  
+                    style={styles.TextInput}  
+                    placeholder="Inserez votre url"  
+                    
+                />  
+      <Text style={styles.Tex}>Entrez une url à reduire</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -18,4 +23,11 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  TextInput: {
+    height: 40,
+    backgroundColor: 'azure',
+     fontSize: 20,
+     marginBotton: 10,
+  }
+ 
 });
